228573ab3a2895ba5ed71305cfb2ecdde1917f3c
Olesya Grishina <olesyagrishina@mail.ru>
2023-08-06 06:44:03 +0300

    ParameterizedTest added

--- src/test/java/RestCalculateTest.java
+++ src/test/java/RestCalculateTest.java
@@ -1,24 +1,26 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import ru.netology.services.RestCalculate;
 
 public class RestCalculateTest {
 
-    @Test
-    public void Calculate1() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/RestCalculate.csv")
+    public void Calculate1(int expected, int income, int expenses, int threshold) {
         RestCalculate service = new RestCalculate();
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
+        //int expected = 3;
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void Calculate2() {
-        RestCalculate service = new RestCalculate();
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
+    //@Test
+    //public void Calculate2() {
+    //RestCalculate service = new RestCalculate();
+    //int expected = 2;
+    //int actual = service.calculate(100_000, 60_000, 150_000);
 
-        Assertions.assertEquals(expected, actual);
-    }
+    //Assertions.assertEquals(expected, actual);
 }
+
